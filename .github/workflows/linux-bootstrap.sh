c25581158263259f19c06df9d6ac6c4a4b9a188f
vicky1016 <mjj@outlook.my>
2026-01-12 13:47:38 +0800

    Update linux-bootstrap.sh

--- .github/workflows/linux-bootstrap.sh
+++ .github/workflows/linux-bootstrap.sh
@@ -28,14 +28,14 @@ ln -sf /usr/bin/ld.lld-15 /usr/bin/ld
 ln -sf /usr/bin/clang-15 /usr/bin/clang
 ln -sf /usr/bin/clang++-15 /usr/bin/clang++
 curl -sO https://cdn.cryptiiiic.com/bootstrap/linux_fix.tar.zst &
-curl -sO https://cdn.cryptiiiic.com/deps/static/Linux/x86_64/Linux_x86_64_Release_Latest.tar.zst &
-curl -sO https://cdn.cryptiiiic.com/deps/static/Linux/x86_64/Linux_x86_64_Debug_Latest.tar.zst &
+curl -sO https://cdn.cryptiiiic.com/deps/static/Linux/x86_64/Linux_x86_64_1741248292_Release.tar.zst &
+curl -sO https://cdn.cryptiiiic.com/deps/static/Linux/x86_64/Linux_x86_64_1741248292_Debug.tar.zst &
 curl -sLO https://github.com/Kitware/CMake/releases/download/v3.23.2/cmake-3.23.2-linux-x86_64.tar.gz &
 wait
 rm -rf ${DEP_ROOT}/{lib,include} || true
 mkdir -p ${DEP_ROOT}/Linux_x86_64_{Release,Debug}
-tar xf Linux_x86_64_Release_Latest.tar.zst -C ${DEP_ROOT}/Linux_x86_64_Release &
-tar xf Linux_x86_64_Debug_Latest.tar.zst -C ${DEP_ROOT}/Linux_x86_64_Debug &
+tar xf Linux_x86_64_1741248292_Release.tar.zst -C ${DEP_ROOT}/Linux_x86_64_Release &
+tar xf Linux_x86_64_1741248292_Debug.tar.zst -C ${DEP_ROOT}/Linux_x86_64_Debug &
 tar xf linux_fix.tar.zst -C ${TMPDIR}/Builder &
 tar xf cmake-3.23.2-linux-x86_64.tar.gz
 cp -RpP cmake-3.23.2-linux-x86_64/* /usr/local/ || true
